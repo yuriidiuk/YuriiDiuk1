24ed22872d4160083ae0ef310354148e02f314a4
yuriidiuk <61579375+yuriidiuk@users.noreply.github.com>
2021-01-21 19:24:14 +0200

    Update app.py

--- laba2/app.py
+++ laba2/app.py
@@ -1,1 +1,60 @@
+import requests
+import ntplib
+from datetime import datetime
 
+
+def get_time_if_url_not_work():
+    c = ntplib.NTPClient()
+    response = c.request('0.ua.pool.ntp.org', version=3)
+    t = datetime.fromtimestamp(response.tx_time).time().strftime('%H:%M:%S %p')
+    d = datetime.fromtimestamp(response.tx_time).date().strftime('%Y-%m-%d')
+    date = {"date": d, "time": t}
+    return date
+
+
+def check_time(d):
+    if "time" in d.keys():
+        print("Time is: ", d['time'])
+    try:
+        print("Date is: ", d['date'])
+    except KeyError:
+        print("No date in response!!!")
+        raise KeyError
+
+
+def main(url=''):
+    if not url:
+        print("No URL passed to function")
+        return False
+
+    try:
+        r = requests.get(url=url)
+    except requests.exceptions.RequestException as e:
+        raise ConnectionError
+
+    if r:
+        check_time(r.json())
+    else:
+        check_time(get_time_if_url_not_work())
+    return True
+
+
+def home_work(url=''):
+    r = requests.get(url=url)
+    d = r.json()
+    str_time = d['time']
+    find_p = str_time.find("P")
+    if find_p == -1:
+        print("Добрий день")
+    else:
+        print("Доброї ночі")
+    return True
+
+
+if name == "main":
+    a = "="*40
+    print(a + "\nРезультат без параметрів: ")
+    main()
+    print(a + "\nРезультат з правильною URL: ")
+    main('http://date.jsontest.com/')
+    home_work('http://date.jsontest.com/')
