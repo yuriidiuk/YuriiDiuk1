a9dceceeec0a1d9f3c53e76f6caf2ead6a59574f
yuriidiuk <61579375+yuriidiuk@users.noreply.github.com>
2021-01-21 23:18:24 +0200

    Update common.py

--- laba2a/modules/common.py
+++ laba2a/modules/common.py
@@ -1,1 +1,19 @@
+import datetime
+import sys
 
+
+def get_current_date():
+    """
+    :return: DateTime object
+    """
+    return datetime.datetime
+
+
+def get_current_platform():
+    """
+    :return: current platform
+    """
+    return sys.platform
+
+def my_func(x):
+	return print(list(range(0, 101, 2)) if x else list(range(1, 101, 2)))
